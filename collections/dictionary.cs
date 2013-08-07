b18fc9a0d334e651a9d9099d892698b0a47593ec
geronimoooooooo <geronimoooooooo@gmail.com>
2013-08-07 14:20:01 +0200

    Update dictionary.cs

--- collections/dictionary.cs
+++ collections/dictionary.cs
@@ -6,3 +6,16 @@ How to replace keys in a dictionary
                 sensorvalues[k] = sensorvalues.ElementAt(i).Value.ToLower().Replace("false", "0");
             }
 --------------------------------------------------------------------------------------------------------
+ Collection was modified; enumeration operation may not execute.
+                foreach (var item in sensorvalues)
+                {
+                    //Das wird nicht gehen, da man item= item.replace( hernehmen müsste. Aber man kann das nicht, da es eine for each schleife ist
+                    item.Value.Replace("True", "1");
+                    //String truee= item.Value.ToLower().Replace("true", "1");
+                    //geht auch nicht, da item.Value nur read only ist
+                    //item.Value = truee;
+
+                    sensorvalues[item.Key]= item.Value.ToLower().Replace("true","1");
+                    sensorvalues[item.Key]= item.Value.ToLower().Replace("false","0");
+                }
+------------------------------------------------------------------------------------------------------
