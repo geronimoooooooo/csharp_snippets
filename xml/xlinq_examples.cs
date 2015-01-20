ca9c28d6c924a9b729162e214c8f539be6839b7a
geronimoooooooo <geronimoooooooo@gmail.com>
2015-01-20 15:18:10 +0100

    Update xlinq_examples.cs

--- xml/xlinq_examples.cs
+++ xml/xlinq_examples.cs
@@ -1,8 +1,17 @@
 XElement cStudent = testXML.Descendants("Student").
 Where(c => c.Attribute("ID").Value.Equals(id.ToString())).FirstOrDefault();
 
-
 var userResults = from u in myDB.Users
                          where u.Username == userName
                          && u.Password == passWord
                          select u;
+
+List<Tutorial> tutorials =
+  (from tutorial in xmlDoc.Descendants("Tutorial")
+   where tutorial.Element("Author").Value == "The Tallest"
+   select new Tutorial
+   {
+     Author = tutorial.Element("Author").Value,
+     Title = tutorial.Element("Title").Value,
+     Date = DateTime.Parse(tutorial.Element("Date").Value),
+   }).ToList<Tutorial>();
