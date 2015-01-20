6f5c2dd197f2e08250f718856ba3a28bb8666143
geronimoooooooo <geronimoooooooo@gmail.com>
2015-01-20 15:13:04 +0100

    Update xlinq_examples.cs

--- xml/xlinq_examples.cs
+++ xml/xlinq_examples.cs
@@ -1,2 +1,8 @@
 XElement cStudent = testXML.Descendants("Student").
 Where(c => c.Attribute("ID").Value.Equals(id.ToString())).FirstOrDefault();
+
+
+var userResults = from u in myDB.Users
+                         where u.Username == userName
+                         && u.Password == passWord
+                         select u;
