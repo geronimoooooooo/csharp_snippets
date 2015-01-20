0d83e523c213d56306c670bc8cdc074ebfed42c4
geronimoooooooo <geronimoooooooo@gmail.com>
2015-01-20 16:06:55 +0100

    Update xlinq_examples.cs

--- xml/xlinq_examples.cs
+++ xml/xlinq_examples.cs
@@ -15,3 +15,12 @@ List<Tutorial> tutorials =
      Title = tutorial.Element("Title").Value,
      Date = DateTime.Parse(tutorial.Element("Date").Value),
    }).ToList<Tutorial>();
+
+//get a single record
+public User GetUser(string userName)
+{    DBNameDataContext myDB = new DBNameDataContext();
+     User user = myDB.Users.Single(u, u.UserName=>userName);
+     return user;
+}
+
+XElement cStudent = testXML.Descendants("Student").Where(c => c.Attribute("ID").Value.Equals(id.ToString())).FirstOrDefault();
