d1f0ea8f60134465a3a71f185756974ae37ec5c5
geronimoooooooo <geronimoooooooo@gmail.com>
2015-02-03 13:00:33 +0100

    Update xlinq_namespaces.cs

--- xml/xlinq_namespaces.cs
+++ xml/xlinq_namespaces.cs
@@ -1,1 +1,4 @@
+http://stackoverflow.com/questions/2340411/use-linq-to-xml-with-xml-namespaces
+https://msdn.microsoft.com/en-us/library/bb387075.aspx
+https://msdn.microsoft.com/en-us/library/bb387069.aspx
 http://stackoverflow.com/questions/6480517/create-xml-doc-by-linq-add-xmlns-xmlnsxsi-to-it
