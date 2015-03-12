5346d4f139e6a308cde5d3f4e9af5e717973380d
geronimoooooooo <geronimoooooooo@gmail.com>
2015-03-12 13:28:27 +0100

    Update linq_examples.cs

--- xml/linq_examples.cs
+++ xml/linq_examples.cs
@@ -1,3 +1,8 @@
+##############################
+string[] numbers = { "0042", "010", "9", "27" };
+int[] nums = numbers.Select(s => Int32.Parse(s)).ToArray();
+int[] nums = numbers.Select(s => Int32.Parse(s)).OrderBy(s => s).ToArray();
+##############################
 var query = people
  .Where (p => p.ID == 1)
  .Select( p => new { p.FirstName, p.LastName } )
